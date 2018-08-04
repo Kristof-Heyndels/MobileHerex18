46b4c0ff07c0d3d31226efac033b8b9eeea06522
Kristof H <kheyndels@gmail.com>
2018-08-04 20:45:09 +0200

    Initial commit

---
+++ app/src/main/java/be/kristofheyndels/mobdev/mobileherex18/Main.java
@@ -0,0 +1,19 @@
+package be.kristofheyndels.mobdev.mobileherex18;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity implements ListFragment.OnFragmentInteractionListener, DetailFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
