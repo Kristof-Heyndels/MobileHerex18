93b376162396ae7f1266dff5df61b50ea59568c9
Kristof H <kheyndels@gmail.com>
2018-08-11 18:07:08 +0200

    Added TAG and URL

--- app/src/main/java/be/kristofheyndels/mobdev/mobileherex18/MainActivity.java
+++ app/src/main/java/be/kristofheyndels/mobdev/mobileherex18/MainActivity.java
@@ -3,8 +3,13 @@ package be.kristofheyndels.mobdev.mobileherex18;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity implements ListFragment.OnFragmentInteractionListener, DetailFragment.OnFragmentInteractionListener{
+
+    public static final String URL = "https://swapi.co/api/";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
