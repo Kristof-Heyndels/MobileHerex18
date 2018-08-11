ebe37fa845d7476efb50931ae18c88112a32b0eb
Kristof H <kheyndels@gmail.com>
2018-08-11 15:34:02 +0200

    Fragment Call to request categories

---
+++ app/src/main/java/Data/Categories.java
@@ -0,0 +1,21 @@
+package Data;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Categories {
+    private static Map<String,String> categoryMap = new HashMap<>();
+
+    public static void addEntry(String key, String value) {
+        String upperKey = key.substring(0,1).toUpperCase() + key.substring(1);
+        categoryMap.put(upperKey, value);
+    }
+
+    public static ArrayList<String> getKeys() {
+        ArrayList<String> categoryList = new ArrayList<>(categoryMap.keySet());
+        Collections.sort(categoryList);
+        return categoryList;
+}
+}
--- app/src/main/java/be/kristofheyndels/mobdev/mobileherex18/ListFragment.java
+++ app/src/main/java/be/kristofheyndels/mobdev/mobileherex18/ListFragment.java
@@ -2,14 +2,42 @@ package be.kristofheyndels.mobdev.mobileherex18;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import Data.Categories;
 
 public class ListFragment extends Fragment {
 
+    private static final String TAG = "ListFragment";
+
+    private Map<String,String> catMap = new HashMap<>();
     private OnFragmentInteractionListener mListener;
 
     public ListFragment() {
@@ -27,6 +55,15 @@ public class ListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Filling Spinner with categories
+        Spinner dropCategory = view.findViewById(R.id.drop_category);
+        populateCategorySpinner(dropCategory);
     }
 
     @Override
@@ -59,5 +96,40 @@ public class ListFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private void populateCategorySpinner(final Spinner spinner) {
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, MainActivity.URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            Iterator<String> keys = jsonResponse.keys();
+
+                           while(keys.hasNext()){
+                               String name = keys.next();
+                               Categories.addEntry(name, jsonResponse.getString(name));
+                           }
+
+                            ArrayAdapter<String> catAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, Categories.getKeys());
+                            spinner.setAdapter(catAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.wtf(TAG,"Response: " + error);
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
